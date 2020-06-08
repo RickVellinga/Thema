aacf64c589c6eb47e98e9f7a3f542eb1b1c49d7f
RickVellinga <55230960+RickVellinga@users.noreply.github.com>
2020-06-08 13:55:41 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html> <?php language_attributes() ?>
+<head>
+  <meta charset="<?php bloginfo('charset') ?>">
+  <meta name="viewport" content="<?php bloginfo('author') ?>">
+  <title><?php bloginfo('name') ?></title>
+</head>
+<body>
+
+<h1><?php bloginfo('name') ?></h1>
