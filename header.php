41341efe7fbab70f5a13e57aafacc577f9d42974
RickVellinga <55230960+RickVellinga@users.noreply.github.com>
2020-06-12 12:13:52 +0200

    Add files via upload
    Ik krijg Kritische Error?!?

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html> <?php language_attributes() ?>
+<head>
+  <meta charset="<?php bloginfo('charset') ?>">
+  <meta name="viewport" content="width=devide-width, initial-scale=1.0">
+  <meta name="author" content="Rick Vellinga">
+  <title><?php bloginfo('name') ?></title>
+  <?php wp_head(); ?>
+</head>
+<body>
+  <header>
+    <nav>
+      <?php wp_nav_menu(array('theme_location' => 'hoofd-menu')); ?>
+    </nav>
+    <h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name')?></a></h1>
+  </header>
